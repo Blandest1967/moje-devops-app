7b2de237f37f86045b65ea905f6af4260b9c68f5
Jan Knotek <knotekjan@seznam.cz>
2025-11-30 19:42:42 +0100

    Testovaci uprava

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Ahoj! Toto běží na mém Ubuntu a brzy to bude v Dockeru."
+    return "Ahoj! Toto je vee 2.0 s automatickzm testem."
 
 if __name__ == '__main__':
     # host='0.0.0.0' je důležité pro pozdější fungování v Dockeru
